ec6967b2eea78763fe2de716d82a58c294655c1a
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-10-06 16:21:37 -0500

    Actividad3

---
+++ Primer Parcial/Práctica_guiada_01/Actividad_3/AgregarComportamientoCondicionalKotlin.kt
@@ -0,0 +1,27 @@
+Actividad 3: 
+// APLICACIONES Móvil.
+// Nombres y Apellidos: Winter Aníbal Meza Jiménez.
+// Curso: Séptimo "A" 2022(2).
+// Práctica Guiada: Hola mundo Kotlin.
+// Fecha: Jueves, 4 de octubre de 2022.
+// Docente: Ing. Edgardo Panchana Flores, Mg.
+fun main() {
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+    // Condiciónes
+    when (rollResult) {
+        luckyNumber -> println("Ganaste!")
+        1 -> println("¡Lo siento mucho! Obtuviste un 1. Inténtalo de nuevo!")
+        2 -> println("Lamentablemente, sacaste un 2. ¡Inténtalo de nuevo!")
+        3 -> println("Desafortunadamente, sacaste un 3. ¡Inténtalo de nuevo!")
+        5 -> println("¡No llores! Obtuviste un 5. Inténtalo de nuevo!")
+        6 -> println("¡Disculpas! Obtuviste un 6. Inténtalo de nuevo!")
+   }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}

6f840150161f30cbb1996c371a9602193d680fbc
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-10-09 19:58:23 -0500

    Debugging Proyecto Completo

--- Primer Parcial/Práctica_guiada_01/Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
+++ Primer Parcial/Práctica_guiada_01/Debugging/app/src/main/java/com/example/debugging/MainActivity.kt
@@ -23,11 +23,11 @@ class MainActivity : AppCompatActivity() {
         division()
     }
     fun logging() {
-        Log.e(TAG, "ERROR: un error grave como un bloqueo de la aplicación")
-        Log.w(TAG, "WARN: advierte sobre la posibilidad de errores graves")
-        Log.i(TAG, "INFO: informar información técnica, como una operación exitosa")
-        Log.d(TAG, "DEBUG: informar información técnica útil para la depuración")
-        Log.v(TAG, "VERBOSE: Más VERBOSE que los registros de depuración")
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
     fun division() {
         val numerator = 60

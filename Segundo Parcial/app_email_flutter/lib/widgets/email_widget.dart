b242f675774346da319ba9eb3647c371b160e0f4
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-12-04 12:34:57 -0500

    Update email_widget.dart

--- Segundo Parcial/app_email_flutter/lib/widgets/email_widget.dart
+++ Segundo Parcial/app_email_flutter/lib/widgets/email_widget.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../model/email.dart';
-
+/*
+APLICACIONES Móvil.
+Nombres y Apellidos: Winter Aníbal Meza Jiménez.
+Curso: Séptimo "A" 2022(2).
+Práctica Guiada 1: Aplicación Mail en Flutter.
+Fecha: Domingo, 4 de diciembre de 2022.
+Docente: Ing. Edgardo Panchana Flores, Mg.
+ */
+// Aqui se declaran las clases con sus respectivas funciones para darle navegavilidad y funcionalidad a los Widgets.
 class EmailWidget extends StatelessWidget {
   final Email email;
   final Function onTap;

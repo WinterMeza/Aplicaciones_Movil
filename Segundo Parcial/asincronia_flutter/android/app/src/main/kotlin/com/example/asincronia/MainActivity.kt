ba48c581bf69b3778fa55b924499014f6173228e
WinterMeza <75690841+WinterMeza@users.noreply.github.com>
2022-12-11 00:14:27 -0500

    Delete

--- Segundo Parcial/asincronia_flutter/android/app/src/main/kotlin/com/example/asincronia/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.asincronia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Segundo Parcial/asincronia_flutter/android/app/src/main/kotlin/com/example/asincronia_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.asincronia_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
